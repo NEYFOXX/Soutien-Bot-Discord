0200fc4ee71da1aecbe1c5af7bee76364cda60a3
NEYFOX <92888341+NEYFOXX@users.noreply.github.com>
2022-03-20 15:53:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,5 @@
 module.exports = {
-    owners: ['922106161153585174'],
-    token: 'OTU1MTA4NDg1NjAxODM3MTY3.Yjc4GA.nIFpmfCU-Wk_zJ6Y3dR8E7UGQYI',
-    prefix: '*',
-
-    database: {
-        host: '127.0.0.1',
-        user: 'root',
-        pass: 'neyfox15',
-        database: 'soutien',
-    }
+    owners: ['Give the owner id of the bot'],
+    token: 'Give your token',
+    prefix: '*'
 }
